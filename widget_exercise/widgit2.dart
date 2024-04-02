4127a4b589060c1dd46fa6c03f77fff4c6352c90
SosiSis <sosinasisay30@gmail.com>
2024-04-02 16:05:10 +0300

    commit

---
+++ widget_exercise/widgit2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LABB extends StatelessWidget {
+  LABB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return column(children: [Row(), Row(), Row()]);
+  }
+}
