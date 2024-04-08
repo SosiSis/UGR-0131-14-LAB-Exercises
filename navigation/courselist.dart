100a7b5fce6d1490eeb991873a5385fb5e34213c
SosiSis <sosinasisay30@gmail.com>
2024-04-08 16:00:05 +0300

    navigation

---
+++ navigation/courselist.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Course {
+  String code;
+  String title;
+  String description;
+  Course({required this.code, required this.title, this.description = ""});
+}
+
+class CoursesListScreen extends StatelessWidget {
+  const CoursesListScreen({required this.courses, required this.onTapped});
+  final List<Course> courses;
+  final ValueChanged<Course> onTapped;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Courses List")),
+      body: ListView(
+        children: [
+          for (var course in courses)
+            ListTile(
+              title: Text(course.title),
+              subtitle: Text(course.code),
+              onTap: () => onTapped(course),
+            ),
+        ],
+      ),
+    );
+  }
+}
