ed16f9f0d377149203ce8d70ee9ada0af655dc79
SosiSis <sosinasisay30@gmail.com>
2024-04-22 14:49:33 +0300

    my first commit

--- Bloc_counter_exercise/bloc_counter_exercise/lib/main.dart
+++ Bloc_counter_exercise/bloc_counter_exercise/lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+// Event
+abstract class CounterEvent {}
+
+class IncrementEvent extends CounterEvent {}
+
+class DecrementEvent extends CounterEvent {}
+
+// State
+class CounterState {
+  final int count;
+
+  CounterState(this.count);
+}
+
+// Bloc
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterState(0));
+
+  @override
+  Stream<CounterState> mapEventToState(CounterEvent event) async* {
+    if (event is IncrementEvent) {
+      yield CounterState(state.count + 1);
+    } else if (event is DecrementEvent) {
+      yield CounterState(state.count - 1);
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -12,62 +41,56 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: CounterPage(),
+      home: BlocProvider(
+        create: (_) => CounterBloc(),
+        child: CounterPage(),
+      ),
     );
   }
 }
 
-class CounterPage extends StatefulWidget {
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
+class CounterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 48.0),
-            ),
-            SizedBox(height: 20.0),
-            Row(
+      body: BlocBuilder<CounterBloc, CounterState>(
+        builder: (context, state) {
+          return Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                TextButton(
-                  onPressed: _decrementCounter,
-                  child: Text('Decrement'),
+                Text(
+                  '${state.count}',
+                  style: TextStyle(fontSize: 48.0),
                 ),
-                SizedBox(width: 20.0),
-                TextButton(
-                  onPressed: _incrementCounter,
-                  child: Text('Increment'),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        counterBloc.add(DecrementEvent());
+                      },
+                      child: Text('Decrement'),
+                    ),
+                    SizedBox(width: 20.0),
+                    TextButton(
+                      onPressed: () {
+                        counterBloc.add(IncrementEvent());
+                      },
+                      child: Text('Increment'),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
