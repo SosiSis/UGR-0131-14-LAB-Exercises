19d8a5dfa0094a4c030f8b8add935b2395c50326
SosiSis <sosinasisay30@gmail.com>
2024-05-13 15:26:06 +0300

    kotlin

--- Kotlin/Variable_exe1.kt
+++ Kotlin/Variable_exe1.kt
@@ -1,18 +1,32 @@
 fun main() {
-    println("Enter the first number: ")
-    val number1 = readLine()!!.toDouble()
+    println("Enter the first number and operation (e.g., 2 +):")
+    val input1 = readLine()
 
-    println("Enter the second number: ")
-    val number2 = readLine()!!.toDouble()
+    println("Enter the second number:")
+    val input2 = readLine()
 
-    println("Enter the operation (addition, subtraction, multiplication, division): ")
-    val operation = readLine()
+    val parts1 = input1?.split(" ")
+    val parts2 = input2?.split(" ")
+
+    if (parts1 == null || parts1.size != 2 || parts2 == null || parts2.size != 1) {
+        println("Invalid input format. Please enter the numbers and operation correctly.")
+        return
+    }
+
+    val number1 = parts1[0].toDoubleOrNull()
+    val operation = parts1[1]
+    val number2 = parts2[0].toDoubleOrNull()
+
+    if (number1 == null || number2 == null) {
+        println("Invalid number format. Please enter valid numbers.")
+        return
+    }
 
     when (operation) {
-        "addition" -> println("Result: ${number1 + number2}")
-        "subtraction" -> println("Result: ${number1 - number2}")
-        "multiplication" -> println("Result: ${number1 * number2}")
-        "division" -> {
+        "+" -> println("Result: ${number1 + number2}")
+        "-" -> println("Result: ${number1 - number2}")
+        "*" -> println("Result: ${number1 * number2}")
+        "/" -> {
             if (number2 != 0.0) {
                 println("Result: ${number1 / number2}")
             } else {
