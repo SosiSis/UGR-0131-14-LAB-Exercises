cf9ff7c2c8cf97312b1262facdb2e2a97dcf219e
SosiSis <sosinasisay30@gmail.com>
2024-05-20 14:57:05 +0300

    android

---
+++ CounterAndroidProject1/app/src/main/java/com/example/androidproject1/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.counterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count = 0;
+    private TextView countTextView;
+    private Button incrementButton, decrementButton, resetButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        countTextView = findViewById(R.id.countTextView);
+        incrementButton = findViewById(R.id.incrementButton);
+        decrementButton = findViewById(R.id.decrementButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                updateCountTextView();
+            }
+        });
+
+        decrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (count > 0) {
+                    count--;
+                }
+                updateCountTextView();
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count = 0;
+                updateCountTextView();
+            }
+        });
+    }
+
+    private void updateCountTextView() {
+        countTextView.setText(String.valueOf(count));
+    }
+}
