8d28cd60417b5da059916c44a24b6515ce22eb32
SosiSis <sosinasisay30@gmail.com>
2024-05-20 22:49:19 +0300

    android

---
+++ JetPackTutorial1/app/src/main/java/com/example/jetpacktutorial1/MainActivity.kt
@@ -0,0 +1,24 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard("Android")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
